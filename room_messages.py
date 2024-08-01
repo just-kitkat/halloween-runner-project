03b2c4c0ddc68655a5735274fe7deb7f80168f4b
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-08-01 07:30:34 +0000

    Halfway done

---
+++ room_messages.py
@@ -0,0 +1,21 @@
+msg = {
+    "kitchen": [
+        "Welcome to the kitchen. It's dark and musty down here.",
+        "Down here and upward, you'll face a ton of different creatures, and be met with various trials for your brave soul.",
+        "But everything down here smells so yummy right?",
+        "No time for your silly stomach! The monsters are coming out soon, so get to work!"
+    ],
+    "main hall": [
+        "Welcome to the main hall. This is the room where it happens",
+        "What’s that coming forward from behind you? You may want to find out. Your curious eyes are calling.",
+        "There’s so many possibilities, but so little time.",
+        "Figure it out, brave soldier. You’ve only got a few moments before those sordid creatures emerge from the horizon.",
+        "Find objects which you can use to help you on your mission. All the best."
+    ],
+    "bathroom": [
+        "Welcome to the bathroom. Here's where things start to get a little sticky.",
+        "In Nanyang Mansion, this is probably the 'cleanest' room.",
+        "Come on! Make use of anything for your benefit in this mission."
+        "All the best!"
+    ]
+}
