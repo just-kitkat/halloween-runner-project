d6c133fe42ec7ed41736cf46d596f4b279396b64
averycuriousfox <107165051+averycuriousfox@users.noreply.github.com>
2024-08-01 07:29:17 +0000

    Added method to display new rooms

--- main.py
+++ main.py
@@ -35,13 +35,14 @@ player = create_player()
 # Main game loop 
 while True:
     # create monster, items in the room, create new doors that link to other rooms
-    current_room = Room()
+    current_room = Dungeon()
     current_room.getMonster()
     player.fightMonster()
     player.collectItems()
     player.breakAction()
+    current_room.displayNewRooms()
     player.chooseRoom()
-    "you can use items, however the more actions you take the higher the chance that monsters would come back in"
+    "you can use items, however the more actions you take the higher the chance that monsters would come back in"# Include this in the player.breakAction() method
     display_break_message() # allow users to eat and use items
 
     display_room_options()
