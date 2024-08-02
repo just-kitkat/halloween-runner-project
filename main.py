d30137b75c41b016d3570ac15eae8f55386aa5d0
averycuriousfox <107165051+averycuriousfox@users.noreply.github.com>
2024-08-02 08:15:07 +0000

    idkcfgv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import room 
-import introduction as intro
+# import introduction as intro
+import player
 """
 Player class:
 - name
@@ -26,26 +27,25 @@ e.g.
 
 # When the game first starts...
 # Introduction paragraph
-intro.start_the_game()
+# intro.start_the_game()
 # Blackout, spawn in dungeon, game loop begins
-display_blackout_message()
 
 # Create player object
-player = create_player()
+player = player.Player()
 # Main game loop 
 while True:
 # create monster, items in the room, create new doors that link to other rooms
     dungeon = room.Dungeon()
     monster = dungeon.get_monster() #call a monster
     fight = player.fight_monster(monster) #player 
-    if fight:
-        player.collect_items()
-    else:
-        player.fail_message()
-        break
-    player.break_action()
+    # if fight:
+    #     player.collect_items()
+    # else:
+    #     player.fail_message()
+    #     break
+    # player.break_action()
     dungeon.clear_room()
-    num_of_rooms = dungeon.get_nums_next_room()
+    num_of_rooms = dungeon.get_nums_next_rooms()
     choose_room = int(input("Which room does your heart desire?:"))
     while (choose_room < num_of_rooms) or (choose_room > num_of_rooms):
         choose_room = int(input("Wrong answer. \nWhich room does your heart desire?:"))
