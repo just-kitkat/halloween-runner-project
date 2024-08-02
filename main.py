9b10755f693d5cd8db127d20cd2b8907e7d0e540
averycuriousfox <107165051+averycuriousfox@users.noreply.github.com>
2024-08-02 07:48:52 +0000

    abit of clean up

--- main.py
+++ main.py
@@ -33,21 +33,17 @@ display_blackout_message()
 # Create player object
 player = create_player()
 # Main game loop 
-class Game:
-    def __init__(self):
-        self.run = True
-        self.player = Player()
-    while True:
-    # create monster, items in the room, create new doors that link to other rooms
-        dungeon = room.Dungeon()
-        monster = dungeon.get_monster() #call a monster
-        fight = player.fight_monster(monster) #player 
-        if fight:
-            player.collect_items()
-        else:
-            player.fail_message()
-            break
-        player.break_action()
-        dungeon.generate_new_rooms()
-        dungeon.display_new_rooms()
-        player.chooseRoom()
+while True:
+# create monster, items in the room, create new doors that link to other rooms
+    dungeon = room.Dungeon()
+    monster = dungeon.get_monster() #call a monster
+    fight = player.fight_monster(monster) #player 
+    if fight:
+        player.collect_items()
+    else:
+        player.fail_message()
+        break
+    player.break_action()
+    dungeon.generate_new_rooms()
+    dungeon.display_new_rooms()
+    player.chooseRoom()
