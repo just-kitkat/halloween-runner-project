9b703ab848c41c469b787f87246c4063092a8258
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-08-02 07:01:34 +0000

    Room Messages Done

--- room_messages.py
+++ room_messages.py
@@ -6,16 +6,30 @@ msg = {
         "No time for your silly stomach! The monsters are coming out soon, so get to work!"
     ],
     "main hall": [
-        "Welcome to the main hall. This is the room where it happens",
+        "Welcome to the main hall. This is the room where it happens.",
         "What’s that coming forward from behind you? You may want to find out. Your curious eyes are calling.",
         "There’s so many possibilities, but so little time.",
-        "Figure it out, brave soldier. You’ve only got a few moments before those sordid creatures emerge from the horizon.",
-        "Find objects which you can use to help you on your mission. All the best."
+        "Figure it out, brave soldier. Those sordid creatures will emerge from the horizon."
     ],
     "bathroom": [
         "Welcome to the bathroom. Here's where things start to get a little sticky.",
         "In Nanyang Mansion, this is probably the 'cleanest' room.",
         "Come on! Make use of anything for your benefit in this mission."
         "All the best!"
-    ]
+    ],
+    "cellar": ["Eeeeeeek! Now comes the scariest part of Nanyang Mansion. The dank, dark, dusty cellar.", 
+               "Monsters here may be the hardest you may ever have to face in your journey here.",
+               "Stakes grow high, for the ominous void grows larger as time passes.", 
+               "There is no escape. You have to fight your way out of this.", 
+               "Keep going. It’s the only way through.",
+               "Good luck…"
+              ],
+    "dungeon": ["Hmm... everything's so isolated here.",
+                "Welcome to the dungeon. Many of the vilest beings have been condemned here for the most heinous acts.",
+                "No one has entered here and lived safely ever since.",
+                "Unleash your bravest soul as you face the monstrous creatures that spontaneously spawn."
+               ],
+    "armoury": ["What a multitude of outfits for the royal servants!",
+               "Makes you feel like a knight when you wear them, right?",
+               "Maybe they'll be of use as you fight the numerous monsters that will appear in this room. You never know."]    
 }
