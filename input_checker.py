463d7ead41273201b058d309faec00f9d52ac640
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-08-07 10:15:01 +0000

    I debugged my one :3

--- input_checker.py
+++ input_checker.py
@@ -4,45 +4,50 @@ def error_msg():
 def check_input_str(question, expected_input, name):
     """
     This function checks whether the input string is in the expected format.
+    question : str
+    expected_input : list of strings in LOWER-CASE letters
+    name = player's name
     """
     ye = True
     while ye:
         print(question)
-        user_input = (f'{name}\'s input: ')
-        user_input = user_input.strip().upper()
+        user_input = input(f'{name}\'s input: ')
+        user_input = user_input.strip().lower()
         if user_input not in expected_input:
-          error_msg()
+            error_msg()
         else:
-          ye = False
-        return user_input
+            ye = False
+            return user_input
 
 def check_input_num(question, expected_input, name):
     """
     This function checks whether the input number is in the expected format.
+    question : str
+    expected_input : list of number strings
+    name = player's name
     """
     ye = True
     while ye:
       print(question)
-      user_input = (f'{name}\'s input: ')
+      user_input = input(f'{name}\'s input: ')
       user_input = user_input.strip()
       if not user_input.isdecimal():
-          error_msg()
+            error_msg()
       elif user_input not in expected_input():
-          error_msg()
-      elif user_input in ['', ' ']:
-          error_msg()
+            error_msg()
       else:
-          ye = False
-    return user_input
+            ye = False
+            return user_input
 
 def check_blankinput(question):
     """
     This function is for open-ended questions that don't require a specific answer.
+    question : str
     """
     ye = True
     while ye:
         print(question)
-        user_input = (f'Player\'s input: ')
+        user_input = input(f'Player\'s input: ')
         if user_input in ['', ' ']:
             error_msg()
         else:
---
+++ names.py
@@ -0,0 +1,11 @@
+import random
+
+def monster_name_generation():
+    first_name = ['Grim', 'Mortis', 'Thorne', 'Vex', 'Hex', 'Nyx', 'Dread', 'Wraith', 'Shade', 'Gloom', 'Ethan', 'Seah', 'Joshua', 'Wong', 'Cho']
+    last_name = ['Nightshade', 'Bloodbane', 'Frostmourne', 'Grimspike', 'Darkwhisper', 'Deathclaw', 'Skullrend', 'Shadowfang', 'Wraithborn', 'Ironshade', 'Wang', 'Eket', 'Zijia', 'Zin']
+    lenf = len(first_name)
+    lenl = len(last_name)
+    num1 = random.randint(0, lenf-1)
+    num2 = random.randint(0, lenl-1)
+    monster_name = f'{first_name[num1]} {last_name[num2]}'
+    return monster_name
