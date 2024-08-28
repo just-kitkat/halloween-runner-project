a7832963273de76a88fd704561e07fcbaa9c366f
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-08-28 16:49:23 +0000

    [feat] First draft of Battle.strike() method

--- battle.py
+++ battle.py
@@ -11,3 +11,16 @@ class Battle:
     def __init__(self, fighter1: player.Player, fighter2: monster.Monster):
         self.fighter1 = fighter1
         self.fighter2 = fighter2
+
+    def strike(self, attacker, defender):
+        """A single blow by an attacker to a defender."""
+        lst = attacker.weapon.attack(defender, defender.armor.protection)
+        bool = lst[0]
+        if bool:
+            print(
+                f"{attacker.name} attacked {defender.name} with \"{attacker.weapon.name}\", dealing {lst[1]} damage"
+            )
+            if lst[2] != 0:
+                print(f"{defender.name}'s armour protected you from {lst[2]} damage!")
+        else:
+            print(f"{attacker.name}\'s attack missed!")
