fa0f1a9644e2515cdc84cfeb2485a59fcfad03d1
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-08-29 04:35:00 +0000

    [refactor] Create Item superclass
    Food, Armor, and Weapon all inherit from Item

--- item.py
+++ item.py
@@ -7,45 +7,49 @@ weapon_data = data.load("weapon.json")
 armor_data = data.load("armor.json")
 
 
-class Armor:
+class Item:
+    """Superclass for all items in the game."""
+    def __init__(self, name: str):
+        self.name = name
+
+    def info(self) -> str:
+        infostr = f"Name: {self.name}"
+        return infostr
+
+class Armor(Item):
 
     def __init__(self, data):
-        self.name = data["name"]
+        super().__init__(data["name"])
         self.protection = data["protection"]
         self.durability = data["durability"]
         self.description = data["description"]
 
     def info(self) -> str:
-        infostr = ""
-        infostr += f"Name: {self.name}"
+        infostr = super().info()
         infostr += f"Protection: {self.protection}"
         infostr += "\n"
         infostr += self.description
         return infostr
 
 
-class Food:
+class Food(Item):
 
     def __init__(self, name: str, health: int):
-        self.name = name
+        super().__init__(name)
         self.health = health
 
-    def info(self) -> str:
-        return ""
 
-
-class Weapon:
+class Weapon(Item):
 
     def __init__(self, data: dict):
-        self.name = data["name"]
+        super().__init__(data["name"])
         self.damage = data["damage"]
         self.accuracy = data["accuracy"]
         self.durability = data["durability"]
         self.description = data["description"]
 
     def info(self) -> str:
-        infostr = ""
-        infostr += f"Name: {self.name}"
+        infostr = super().info()
         infostr += f"Damage: {self.damage}"
         infostr += f"Accuracy: {self.accuracy}"
         infostr += "\n"
